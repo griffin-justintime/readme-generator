d8c7f7623f2128159bf62c1cbb557444e64f3d14
Justin Griffin <griffin.justintime@gmail.com>
2021-04-08 11:28:20 -0400

    reconfigured files/added some questions

--- index.js
+++ index.js
@@ -1,2 +1,41 @@
+// TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
+
+// TODO: Create an array of questions for user input
+const questions = [
+    {
+        type: 'input',
+      name: 'name',
+      message: 'What is the project title?',
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Enter a description.',
+    },
+    {
+        type: 'input',
+        name: 'instructions.',
+        message: 'Enter installation instructions.',
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Enter usage information.',
+    },
+    {
+        type: 'input',
+        name: 'guidelines',
+        message: 'Enter contribution guidelines.',
+    }
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
